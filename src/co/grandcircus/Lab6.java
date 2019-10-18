a65517b4cf61d144911dd60e54bb0a0d49d53e23
jacklearnsjava <jack.p.hagen@gmail.com>
2019-10-18 00:46:21 -0400

    initial commit

---
+++ src/co/grandcircus/Lab6.java
@@ -0,0 +1,65 @@
+package co.grandcircus;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lab6 {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Welcome to Grand Circus Casino");
+		System.out.println();
+		System.out.println("How many sides do your dice have?: ");
+		int dieSides1 = scnr.nextInt();
+		String userChar = scnr.nextLine();
+		int numRolls = 1;
+
+		System.out.println("Press any key to roll: ");
+		userChar = scnr.nextLine();
+
+		System.out.println("Roll " + numRolls + ":");
+		System.out.println(generateRandomDiceRoll(dieSides1));
+		System.out.println(generateRandomDiceRoll(dieSides1));
+
+		System.out.println();
+		System.out.println("Do you want to roll again? (y/n)");
+		userChar = scnr.nextLine();
+		numRolls = numRolls + 1;
+
+		System.out.println("Roll " + numRolls + ":");
+		System.out.println(generateRandomDiceRoll(dieSides1));
+		System.out.println(generateRandomDiceRoll(dieSides1));
+
+		while (userChar.equalsIgnoreCase("y") || userChar.equalsIgnoreCase("Yes")) {
+
+			System.out.println();
+			System.out.println("Do you want to roll again? (y/n)");
+			userChar = scnr.nextLine();
+			numRolls = numRolls + 1;
+
+			System.out.println("Roll " + numRolls + ":");
+			System.out.println(generateRandomDiceRoll(dieSides1));
+			System.out.println(generateRandomDiceRoll(dieSides1));
+
+			while (userChar.equalsIgnoreCase("n") || userChar.equalsIgnoreCase("No")) {
+				System.out.println("Thanks for playing!");
+				break;
+			}
+
+		}
+
+		scnr.close();
+	}
+
+	public static int generateRandomDiceRoll(int numSides) {
+
+		return (int) Math.ceil(Math.random() * numSides);
+	}
+
+	public static int generateRandomDiceRoll2(int numSides) {
+
+		Random r = new Random();
+		return r.nextInt(numSides);
+	}
+
+}
